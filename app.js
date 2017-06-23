230ddfd6e57ed61e056e5a99a66ed20bcdc008e9
MUHAMMAD HARIS <MUHAMMAD HARIS>
2017-06-23 20:52:53 +0500

    A tribute page on Pakistan Cricket

---
+++ app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("sub ").addClass("animated fadeInUp");
+  $(".main").addClass("animated fadeIn");
+  $(".text").addClass("animated fadeIn");
+  $(".image").addClass("animated pulse");
+  });
